f764f725260c24d1d9f2abd52d39ea2012e2e38a
gzra50 <yousseftag50@gmail.com>
2025-04-28 18:41:34 +0300

    added the js for the pop up masseges

--- js/registration.js
+++ js/registration.js
@@ -1,10 +1,38 @@
-function openPopup(event) {
-    event.preventDefault(); 
+const password = document.getElementById('password');
+const conpassword = document.getElementById('conpassword');
+const registrationForm = document.getElementById('registrationForm');
+
+function openPopup() {
     document.getElementById('popup').style.display = 'flex';
+}
+
+function openMismatchPopup() {
+    document.getElementById('matching-passwords').style.display = 'flex';
 }
 
 function closePopup() {
     document.getElementById('popup').style.display = 'none';
+    document.getElementById('matching-passwords').style.display = 'none';
 }
 
-document.getElementById('registrationForm').addEventListener('submit', openPopup);
+function whichEvent(event) {
+    event.preventDefault();
+
+      const fullName = document.getElementById('full_name').value;
+      const email = document.getElementById('email').value;
+      const dob = document.getElementById('dob').value;
+      const gender = document.querySelector('input[name="gender"]:checked');
+  
+      if (!fullName || !email || !dob || !gender || !password || !conpassword) {
+          alert("Please fill all required fields!");
+          return;
+      }
+    
+    if (password.value === conpassword.value) {
+        openPopup();
+    } else {
+        openMismatchPopup();
+    }
+}
+
+registrationForm.addEventListener('submit', whichEvent);
