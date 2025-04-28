3fd5675d1c3012520bac49d414ac23a516838ffa
gzra50 <yousseftag50@gmail.com>
2025-04-28 18:09:33 +0300

    added the popup massege for the submit

--- js/registration.js
+++ js/registration.js
@@ -1,4 +1,10 @@
-// const button = document.getElementById("submit-btn");
-// button.addEventListener('click' , function(){
-//     window.location.href = 'bodyFooter.html'
-// });
+function openPopup(event) {
+    event.preventDefault(); 
+    document.getElementById('popup').style.display = 'flex';
+}
+
+function closePopup() {
+    document.getElementById('popup').style.display = 'none';
+}
+
+document.getElementById('registrationForm').addEventListener('submit', openPopup);
