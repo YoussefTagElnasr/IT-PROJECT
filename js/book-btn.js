aaaa4bee20888a106cb2bbac1b493f4955d87910
JoeDarkness <yousefahmedkamaleldin@gmail.com>
2025-05-07 15:47:16 +0200

    ofaldo 3aleh

--- js/book-btn.js
+++ js/book-btn.js
@@ -1,16 +1,16 @@
 document.querySelector('.book-btn').onclick = (e) => {
-    e.preventDefault();
+    // e.preventDefault();
     document.querySelector('.form-bg').classList.add('visible');
 };
 
 document.querySelector('.book-form').onsubmit = (e) => {
-    e.preventDefault();
+    // e.preventDefault();
     document.querySelector('.form-bg').classList.remove('visible');
     document.querySelector('.book-msg').classList.add('visible');
 };
 
 document.querySelector('.cancel-btn').onclick = (e) => {
-    e.preventDefault();
+    // e.preventDefault();
     document.querySelector('.form-bg').classList.remove('visible');
 };
 
